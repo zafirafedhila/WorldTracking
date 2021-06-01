e1f6f3ccd176804d6c07a5c2def5ea735c637b87
Zafira Fedhila <zafirafedhila@gmail.com>
2021-06-01 18:05:19 +0700

    Initial Commit

---
+++ World Tracking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  World Tracking
+//
+//  Created by Zafira Fedhila on 01/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
